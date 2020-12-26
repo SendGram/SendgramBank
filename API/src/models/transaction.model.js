99b477e43d9b3a63af4ca63813c33be011917273
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-26 18:00:42 +0100

    FIX: create Transaction model

--- API/src/models/transaction.model.js
+++ API/src/models/transaction.model.js
@@ -39,3 +39,5 @@ function isEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+module.exports = mongoose.model('Transaction', transactionSchema);
