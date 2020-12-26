bfb4e141f2d630212e6d94bb9cf90095c51fd8df
Alessandro Boseggia <alessandro.boseggia02@gmail.com>
2020-12-26 17:37:40 +0100

    ADD: Transaction Router #3

---
+++ API/src/routes/transaction.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const controller = require('../controller/transaction.controller')
+
+/**
+ * @api {post} transaction/new Transaction
+ * @apiDescription Create new Transaction
+ * @apiVersion 1.0.0
+ * @apiName New Transaction
+ * @apiGroup Transaction
+ * @apiPermission public
+ *
+ * @apiParam  {String{3..}}             jwt         Json Web Token
+ * @apiParam  {String{6..128}}          sender      User's email of sender
+ * @apiParam  {String{6..128}}          beneficiary  User's email of sender
+ * @apiParam  {Number}                  ammount     Transaction ammount
+ * 
+ * @apiSuccess (Created 201)
+ *
+ * @apiError (Bad Request 400)  ValidationError  Some parameters may contain invalid values
+ */
+router.post('/new', controller.newTransaction);
+
+module.exports = router;
