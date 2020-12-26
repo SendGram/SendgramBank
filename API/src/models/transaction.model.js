62aa5d7410d615e48dd02818c59bfa0101d6d457
Alessandro Boseggia <alessandro.boseggia02@gmail.com>
2020-12-26 17:16:48 +0100

    ADD: transaction model

---
+++ API/src/models/transaction.model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const APIError = require('../utils/apiError');
+const httpStatus = require('http-status');
+const Double = require('@mongoosejs/double');
+const User = require('../models/user.model');
+
+
+const transactionSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    mittente: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    destinatario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    importo: {
+        type: Double,
+        required: true,
+    }
+});
+
+transactionSchema.pre('save', async function(next) {
+    if (isEmail(this.mittente)) {
+        const user = await User.findOne({ "email": this.mittente });
+        this.mittente = user._id;
+    }
+    if (isEmail(this.destinatario)) {
+        const user = await User.findOne({ "email": this.destinatario });
+        this.destinatario = user._id;
+    }
+    return next();
+});
+
+function isEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
