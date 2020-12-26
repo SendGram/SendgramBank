d2674f4efb05664df69288f408be0c559045a67c
Alessandro Boseggia <alessandro.boseggia02@gmail.com>
2020-12-26 17:37:12 +0100

    ALTER: Add transaction router #3

--- API/app.js
+++ API/app.js
@@ -5,9 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const httpStatus = require('http-status');
 
-var indexRouter = require('./src/routes/index');
-var usersRouter = require('./src/routes/users');
-var authRouter = require('./src/routes/auth');
+const indexRouter = require('./src/routes/index');
+const usersRouter = require('./src/routes/users');
+const authRouter = require('./src/routes/auth');
+const transactionRouter = require('./src/routes/transaction');
 var env = process.env.NODE_ENV || 'developent';
 
 const mongoose = require('mongoose');
