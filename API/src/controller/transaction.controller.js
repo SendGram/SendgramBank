e60eccae0bfe7c79f313c2f9b622036295359e51
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-26 18:52:51 +0100

    Security FIX: use email in jwt instead of email in http body #3

--- API/src/controller/transaction.controller.js
+++ API/src/controller/transaction.controller.js
@@ -5,8 +5,8 @@ const Transaction = require('../models/transaction.model');
 
 exports.newTransaction = async(req, res, next) => {
     try {
-        const { sender, beneficiary, ammount } = req.body;
-        const transaction = await Transaction.newTransaction(sender, beneficiary, ammount);
+        const { beneficiary, ammount } = req.body;
+        const transaction = await Transaction.newTransaction(req.session.email, beneficiary, ammount);
 
         const saved = await transaction.save();
         res.status(201).json(saved);
