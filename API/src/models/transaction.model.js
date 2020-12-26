7b6fa496584523b5cfda9159ec18d758d39722dc
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-26 18:16:25 +0100

    NEW: newTransaction static method #3

--- API/src/models/transaction.model.js
+++ API/src/models/transaction.model.js
@@ -23,21 +23,35 @@ const transactionSchema = mongoose.Schema({
     }
 });
 
-transactionSchema.pre('save', async function(next) {
-    if (isEmail(this.sender)) {
-        const user = await User.findOne({ "email": this.sender });
-        this.sender = user._id;
+transactionSchema.statics.newTransaction = async function(sender, beneficiary, ammount) {
+    const err = {
+        status: httpStatus.UNAUTHORIZED,
+        isPublic: true,
+    };
+    if (isEmail(sender)) {
+        const user = await User.findOne({ "email": sender });
+        if (!user) throw new APIError({...err, message: 'Sender email is wrong' });
+        sender = user._id;
     }
-    if (isEmail(this.beneficiary)) {
-        const user = await User.findOne({ "email": this.beneficiary });
-        this.beneficiary = user._id;
+    if (isEmail(beneficiary)) {
+        const user = await User.findOne({ "email": beneficiary });
+        if (!user) throw new APIError({...err, message: 'Beneficiary email is wrong' });
+        beneficiary = user._id;
     }
-    return next();
-});
+
+    return await new Transaction({
+        _id: new mongoose.Types.ObjectId(),
+        sender,
+        beneficiary,
+        ammount
+    });
+};
+
 
 function isEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
-module.exports = mongoose.model('Transaction', transactionSchema);
+const Transaction = mongoose.model('Transaction', transactionSchema);
+module.exports = Transaction;
