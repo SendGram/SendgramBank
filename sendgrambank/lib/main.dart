7fa9107445d3e2ec334dc6eaed5ef5db8db220cf
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2021-01-01 20:00:30 +0100

    Flutter main Dependency Injection BLoC

--- sendgrambank/lib/main.dart
+++ sendgrambank/lib/main.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sendgrambank/pages/HomePage.dart';
+import 'package:sendgrambank/pages/LoginPage.dart';
+import 'package:sendgrambank/services/AuthService.dart';
+import 'blocs/auth/auth.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() =>
+    //Dependency injection
+    runApp(
+      RepositoryProvider<AuthService>(
+        create: (context) {
+          return APIAuthenticationService();
+        },
+        child: BlocProvider<AuthenticationBloc>(
+          create: (context) {
+            final authService = RepositoryProvider.of<AuthService>(context);
+            return AuthenticationBloc(authService)..add(AppLoaded());
+          },
+          child: MyApp(),
+        ),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SendgramBank',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+          primaryColor: Color(0xffcfcfcf), hintColor: Color(0xff9e9e9e)),
+      home: BlocBuilder<AuthenticationBloc, AuthState>(
+        builder: (context, state) {
+          if (state is AuthenticatedState) {
+            //Utente già autenticato, mostro HomePage
+            return HomePage();
+          }
+          return LoginPage();
+        },
       ),
-      home: Text("jhs"),
     );
   }
 }
