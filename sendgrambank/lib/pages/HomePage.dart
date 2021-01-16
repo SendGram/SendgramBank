0a6e811d9e006b64d88b625e7f4d3289a1d576b1
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2021-01-16 18:34:43 +0100

    ADD: user in HomePage

--- sendgrambank/lib/pages/HomePage.dart
+++ sendgrambank/lib/pages/HomePage.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:sendgrambank/models/User.dart';
 
 class HomePage extends StatelessWidget {
+  final User currentUser;
+
+  const HomePage({Key key, this.currentUser}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("HomePage"));
+    return Container(child: Text(currentUser.lastname));
   }
 }
