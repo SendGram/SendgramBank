47d9ccd9cd9d066ac80c2d72bb370e93d7dc72bd
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2021-03-19 16:36:07 +0100

    ADD-UI: available amount, layout and graph

--- sendgrambank/lib/pages/HomePage.dart
+++ sendgrambank/lib/pages/HomePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sendgrambank/models/User.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class HomePage extends StatelessWidget {
   final User currentUser;
@@ -7,11 +9,12 @@ class HomePage extends StatelessWidget {
   const HomePage({Key key, this.currentUser}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
+    print(size.width);
     return Scaffold(
-        backgroundColor: Color(0xffD9D9D9),
-        body: SafeArea(
-            child: Column(
+      backgroundColor: Color(0xffD9D9D9),
+      body: SafeArea(
+        child: Column(
           children: [
             Row(
               children: [
@@ -27,16 +30,42 @@ class HomePage extends StatelessWidget {
                     decoration: BoxDecoration(color: Color(0xFFF9F8F8)),
                     child: Row(
                       children: [
-                        Align(
-                          alignment: AlignmentDirectional.center,
-                          child: Text("coso"),
+                        Container(
+                          margin: EdgeInsets.only(left: 35),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    height: 35,
+                                    child: Text("500.00",
+                                        style: GoogleFonts.roboto(
+                                            textStyle: TextStyle(
+                                                fontSize: 35,
+                                                fontWeight: FontWeight.w500,
+                                                color: Color(0xff494949)))),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 10),
+                                  ),
+                                  Text("EUR",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w300)),
+                                ],
+                              ),
+                              Text("Saldo")
+                            ],
+                          ),
                         ),
                         Expanded(
                           child: Align(
                             alignment: Alignment.centerRight,
                             child: Container(
                               width: 128,
-                              height: 30,
+                              height: 40,
                               child: ElevatedButton(
                                 autofocus: false,
                                 onPressed: () {},
@@ -75,15 +104,140 @@ class HomePage extends StatelessWidget {
               ],
             ),
             Expanded(
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Container(
-                  width: 130,
-                  decoration: BoxDecoration(color: Color(0xFFF9F8F8)),
-                ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 130,
+                    decoration: BoxDecoration(color: Color(0xFFF9F8F8)),
+                  ),
+
+                  //internal space
+                  Content()
+                ],
               ),
             )
           ],
-        )));
+        ),
+      ),
+    );
   }
+}
+
+class Content extends StatelessWidget {
+  const Content({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: SingleChildScrollView(
+          child: Wrap(
+            spacing: 20,
+            runSpacing: 20,
+            children: [
+              Container(
+                height: 200,
+                width: (size.width > 1000)
+                    ? (size.width - 130 - 40) / 2 - 20
+                    : (size.width - 130 - 40),
+                child: FirstGraph(),
+                decoration: BoxDecoration(
+                  color: Color(0xffF7F8F8),
+                ),
+              ),
+              Container(
+                height: 200,
+                width: (size.width > 1000)
+                    ? (size.width - 130 - 40) / 2 - 20
+                    : (size.width - 130 - 40),
+                child: Text("2"),
+                decoration: BoxDecoration(
+                  color: Color(0xffF7F8F8),
+                ),
+              ),
+              Container(
+                width: (size.width - 130 - 40),
+                child: LargeGraph(),
+                decoration: BoxDecoration(
+                  color: Color(0xffF7F8F8),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//da rinominare una volta scelti i dati
+class FirstGraph extends StatelessWidget {
+  const FirstGraph({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCartesianChart(
+      // Initialize category axis
+      primaryXAxis: CategoryAxis(),
+      series: <LineSeries<TransactionDataExample, String>>[
+        LineSeries<TransactionDataExample, String>(
+            // Bind data source
+            dataSource: <TransactionDataExample>[
+              TransactionDataExample('Gen', 35),
+              TransactionDataExample('Feb', 28),
+              TransactionDataExample('Mar', 34),
+              TransactionDataExample('Apr', 32),
+              TransactionDataExample('Mag', 40)
+            ],
+            xValueMapper: (TransactionDataExample t, _) => t.year,
+            yValueMapper: (TransactionDataExample t, _) => t.transactions),
+      ],
+    );
+  }
+}
+
+//da rinominare una volta scelti i dati
+class LargeGraph extends StatelessWidget {
+  const LargeGraph({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCartesianChart(
+      tooltipBehavior: TooltipBehavior(enable: true),
+      title: ChartTitle(text: "Tot entrare e uscite (spline chart)"),
+      primaryYAxis: NumericAxis(minimum: 20),
+      primaryXAxis: CategoryAxis(),
+      series: <SplineSeries<TransactionDataExample, String>>[
+        SplineSeries<TransactionDataExample, String>(
+          // Bind data source
+          dataSource: <TransactionDataExample>[
+            TransactionDataExample('Gen', 35),
+            TransactionDataExample('Feb', 28),
+            TransactionDataExample('Mar', 34),
+            TransactionDataExample('Apr', 32),
+            TransactionDataExample('Mag', 40)
+          ],
+          xValueMapper: (TransactionDataExample t, _) => t.year,
+          yValueMapper: (TransactionDataExample t, _) => t.transactions,
+          markerSettings: MarkerSettings(isVisible: true),
+        )
+      ],
+    );
+  }
+}
+
+class TransactionDataExample {
+  TransactionDataExample(this.year, this.transactions);
+  final String year;
+  final int transactions;
 }
