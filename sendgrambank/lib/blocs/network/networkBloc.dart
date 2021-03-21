4ea589c9e9665b7c8846f3b4e09e41a647ffb01b
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2021-03-21 16:34:20 +0100

    retry network connectivity

--- sendgrambank/lib/blocs/network/networkBloc.dart
+++ sendgrambank/lib/blocs/network/networkBloc.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
 import 'networkState.dart';
 import 'networkEvent.dart';
 
@@ -10,7 +13,20 @@ class NetworkBloc extends Bloc<NetworkEvent, NetworkState> {
     if (event == NetworkEvent.NetworkFailEvent) {
       yield NetworkState.NetworkError;
     } else {
+      yield* _retry();
+    }
+  }
+
+  Stream<NetworkState> _retry() async* {
+    Response response;
+    try {
+      response = await Dio().get("http://127.0.0.1:3000/",
+          options: Options(headers: {
+            HttpHeaders.contentTypeHeader: "application/json",
+          }));
       yield NetworkState.NetworkAvailable;
+    } on DioError catch (e) {
+      yield NetworkState.NetworkError;
     }
   }
 }
