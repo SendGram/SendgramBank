c0620d825189bf4faa92fa992e6cd640dc04fd99
AlessandroBoseggia <alessandro.boseggia02@gmail.com>
2021-03-26 21:48:15 +0100

    ADD: create new transaction in TransactionBloc

--- sendgrambank/lib/blocs/Transaction/Transaction_bloc.dart
+++ sendgrambank/lib/blocs/Transaction/Transaction_bloc.dart
@@ -1,15 +1,30 @@
 import 'package:bloc/bloc.dart';
+import 'package:sendgrambank/models/User.dart';
+import 'package:sendgrambank/services/TransactionService.dart';
 import 'index.dart';
 
 class TransactionBloc extends Bloc<TransactionEvent, TransactionState> {
-  TransactionBloc() : super(TransactionInitialState());
+  final TransactionService _transactionService;
+
+  TransactionBloc(TransactionService transactionService)
+      : _transactionService = transactionService,
+        super(TransactionInitialState());
 
   @override
   Stream<TransactionState> mapEventToState(
     TransactionEvent event,
   ) async* {
     if (event is NewTransactionEvent) {
-      print(event.beneficiary + " " + event.amount);
+      createNewTransaction(event.sender, event.beneficiary, event.amount);
+    }
+  }
+
+  void createNewTransaction(User sender, String beneficiary, String amount) {
+    try {
+      double doubleAmount = double.parse(amount);
+      _transactionService.newTransaction(sender, beneficiary, doubleAmount);
+    } on FormatException catch (e) {
+      print(e);
     }
   }
 }
