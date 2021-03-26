84e5fc629b3316446ce26d33b4c3c9668bc6a49c
AlessandroBoseggia <alessandro.boseggia02@gmail.com>
2021-03-26 21:54:34 +0100

    ADD: getAmount and getTransaction routes

--- API/src/routes/users.js
+++ API/src/routes/users.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
+router.get("/getAmount", function (req, res, next) {
+    res.send("respond with a resource");
+});
+
+router.get("/getTransaction", function (req, res, next) {
     res.send("respond with a resource");
 });
 
