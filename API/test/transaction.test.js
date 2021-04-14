edfe8eaec37f338683298cea7b6146c17f648461
Ale041 <alessandro.boseggia02@gmail.com>
2021-04-14 22:11:38 +0200

    ADD: more test for transaction

--- API/test/transaction.test.js
+++ API/test/transaction.test.js
@@ -38,4 +38,51 @@ module.exports = () => {
                 done();
             });
     });
+
+    step("creo transazione verso email inesistente", async (done) => {
+        chai.request(server)
+            .post("/transaction/new")
+            .set("jwt", sharedData.jwt)
+            .send({ beneficiary: "test@example.com", amount: 10 })
+            .end((err, res) => {
+                res.should.have.status(406);
+                done();
+            });
+    });
+
+    step("creo transazione verso email non vailda", async (done) => {
+        chai.request(server)
+            .post("/transaction/new")
+            .set("jwt", sharedData.jwt)
+            .send({ beneficiary: "invalid email", amount: 10 })
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            });
+    });
+
+    step("creo transazione verso amount non vaildo", async (done) => {
+        chai.request(server)
+            .post("/transaction/new")
+            .set("jwt", sharedData.jwt)
+            .send({
+                beneficiary: "email1@example.com",
+                amount: "10h",
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            });
+    });
+
+    step("creo transazione", async (done) => {
+        chai.request(server)
+            .post("/transaction/new")
+            .set("jwt", sharedData.jwt)
+            .send({ beneficiary: "email1@example.com", amount: 10 })
+            .end((err, res) => {
+                res.should.have.status(406);
+                done();
+            });
+    });
 };
