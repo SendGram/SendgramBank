55ee992b008d4738b7a7813bcd82264749b71cf7
Ale041 <alessandro.boseggia02@gmail.com>
2021-04-14 22:09:47 +0200

    ADD: TransactionErrorState class

--- sendgrambank/lib/blocs/Transaction/Transaction_state.dart
+++ sendgrambank/lib/blocs/Transaction/Transaction_state.dart
@@ -1,3 +1,9 @@
 abstract class TransactionState {}
 
 class TransactionInitialState extends TransactionState {}
+
+class TransactionErrorState extends TransactionState {
+  String message;
+  String position;
+  TransactionErrorState({this.message, this.position});
+}
