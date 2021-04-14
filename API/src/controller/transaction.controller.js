f0a550de968f5f363a93001a506472262610d131
Ale041 <alessandro.boseggia02@gmail.com>
2021-04-14 22:24:26 +0200

    ALTER: API transaction.controller
    Updating the account of the beneficiary and the sender when a transaction is made

--- API/src/controller/transaction.controller.js
+++ API/src/controller/transaction.controller.js
@@ -1,11 +1,35 @@
 const Transaction = require("../models/transaction.model");
+const User = require("../models/user.model");
+const APIError = require("../utils/apiError");
 
 exports.newTransaction = async (req, res, next) => {
     try {
         const { beneficiary, amount } = req.body;
+        const senderEmail = req.session.email;
+        const beneficiaryEmail = beneficiary;
+
+        const senderBalance = await User.findUserBalanceByEmail(senderEmail);
+        const beneficaryBalance = await User.findUserBalanceByEmail(
+            beneficiaryEmail
+        );
+
+        if (senderBalance < amount) {
+            throw new APIError({
+                statusCode: 406,
+                isPublic: true,
+                message: "insufficent balance",
+            });
+        }
+
+        const newSenderBalance = senderBalance - amount;
+        const newBeneficaryBalance = beneficaryBalance + amount;
+
+        await User.updateBalanceByEmail(senderEmail, newSenderBalance);
+        await User.updateBalanceByEmail(beneficiaryEmail, newBeneficaryBalance);
+
         const transaction = await Transaction.newTransaction(
-            req.session.email,
-            beneficiary,
+            senderEmail,
+            beneficiaryEmail,
             amount
         );
 
