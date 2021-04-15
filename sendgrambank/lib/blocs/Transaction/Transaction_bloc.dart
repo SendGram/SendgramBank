1df52b764db92759d3220e5f30bc8f439d8d175c
Ale041 <alessandro.boseggia02@gmail.com>
2021-04-15 08:19:16 +0200

    ALTER: transaction bloc
    ADD: added validations for email and amount
    ADD: error management by validators and transaction service

--- sendgrambank/lib/blocs/Transaction/Transaction_bloc.dart
+++ sendgrambank/lib/blocs/Transaction/Transaction_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:sendgrambank/exceptions/TransactionException.dart';
 import 'package:sendgrambank/models/User.dart';
 import 'package:sendgrambank/services/TransactionService.dart';
 import 'index.dart';
+import 'package:sendgrambank/validator/TransactionValidator.dart';
 
 class TransactionBloc extends Bloc<TransactionEvent, TransactionState> {
   final TransactionService _transactionService;
@@ -15,16 +17,25 @@ class TransactionBloc extends Bloc<TransactionEvent, TransactionState> {
     TransactionEvent event,
   ) async* {
     if (event is NewTransactionEvent) {
-      createNewTransaction(event.sender, event.beneficiary, event.amount);
+      yield* createNewTransaction(
+          event.sender, event.beneficiary, event.amount);
     }
   }
 
-  void createNewTransaction(User sender, String beneficiary, String amount) {
+  Stream<TransactionState> createNewTransaction(
+      User sender, String beneficiary, String amount) async* {
     try {
       double doubleAmount = double.parse(amount);
-      _transactionService.newTransaction(sender, beneficiary, doubleAmount);
+      beneficiary = emailValidator(beneficiary);
+
+      await _transactionService.newTransaction(
+          sender, beneficiary, doubleAmount);
     } on FormatException catch (e) {
-      print(e);
+      yield TransactionErrorState(
+          message: "Invalid amount", position: "amount");
+    } catch (e) {
+      print("error");
+      yield TransactionErrorState(message: e.message, position: e.position);
     }
   }
 }
