0a1a6c5c1cb2b23a6c8c83545bc5b81879185f4f
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2021-04-17 17:20:01 +0200

    ADD: loading and completed states

--- sendgrambank/lib/blocs/Transaction/Transaction_bloc.dart
+++ sendgrambank/lib/blocs/Transaction/Transaction_bloc.dart
@@ -1,5 +1,5 @@
 import 'package:bloc/bloc.dart';
-import 'package:sendgrambank/exceptions/TransactionException.dart';
+import 'package:dio/dio.dart';
 import 'package:sendgrambank/models/User.dart';
 import 'package:sendgrambank/services/TransactionService.dart';
 import 'index.dart';
@@ -19,6 +19,8 @@ class TransactionBloc extends Bloc<TransactionEvent, TransactionState> {
     if (event is NewTransactionEvent) {
       yield* createNewTransaction(
           event.sender, event.beneficiary, event.amount);
+    } else {
+      yield TransactionInitialState();
     }
   }
 
@@ -28,14 +30,20 @@ class TransactionBloc extends Bloc<TransactionEvent, TransactionState> {
       double doubleAmount = double.parse(amount);
       beneficiary = emailValidator(beneficiary);
 
+      yield TransactionLoadingState();
       await _transactionService.newTransaction(
           sender, beneficiary, doubleAmount);
+
+      yield TransactionCompletedState();
     } on FormatException catch (e) {
       yield TransactionErrorState(
           message: "Invalid amount", position: "amount");
     } catch (e) {
-      print("error");
-      yield TransactionErrorState(message: e.message, position: e.position);
+      if (e is DioError) {
+        yield TransactionErrorState();
+      } else {
+        yield TransactionErrorState(message: e.message, position: e.position);
+      }
     }
   }
 }
